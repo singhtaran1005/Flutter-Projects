80f3182ea8207d70eaf14f4c9d2afb7ab1353ce2
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-04-08 23:26:45 +0530

    completed.

--- Flutter_QuizApp/lib/questions.dart
+++ Flutter_QuizApp/lib/questions.dart
@@ -2,9 +2,6 @@ class Question {
   String questionText;
   bool quesanswer;
 
-  Question({String q, bool a}) {
-    questionText = q;
-    quesanswer = a;
-  }
-  
+  Question(this.questionText, this.quesanswer) ;
+
 }
