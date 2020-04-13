2b86713592b02f741f69c6103aa2533fbe858237
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-04-13 23:39:32 +0530

    commit.

--- BMI calculator/bmi_calculator/lib/input_page.dart
+++ BMI calculator/bmi_calculator/lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,12 +20,12 @@ class _InputPageState extends State<InputPage> {
                 children: <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                 ],
@@ -32,7 +33,7 @@ class _InputPageState extends State<InputPage> {
             ),
             Expanded(
               child: ReusableCard(
-                colour: Color(0xFF1D1E33),
+                colour: activeCardColor,
               ),
             ),
             Expanded(
@@ -40,16 +41,22 @@ class _InputPageState extends State<InputPage> {
                 children: <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                 ],
               ),
+            ),
+            Container(
+              color: Color(0xFFEB1555) ,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight ,
             ),
           ],
         ));
@@ -57,13 +64,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour,this.cardchild});
 
   final Color colour;
+  final Widget cardchild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardchild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
