3d2a1f9e283072ec863170463f641b767f776ac3
Taranpreet Singh Chabbra <53126276+singhtaran1005@users.noreply.github.com>
2020-04-16 17:37:29 +0530

    Create cosntants.dart

---
+++ BMI calculator/bmi_calculator/lib/cosntants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(
+ fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+);
+const knumber = TextStyle(
+                            fontSize: 50.0, fontWeight: FontWeight.w900 );
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const inactivecardcolor = Color(0xFF111328);
